1af19254c84d18ccfc0720c97e55d4311992bf92
Willy Brauner <mail@willybrauner.com>
2021-11-09 11:47:26 +0100

    Render from php template

--- dist/front/index.php
+++ dist/front/index.php
@@ -4,9 +4,16 @@
     <meta charset="UTF-8" />
     <title>App PHP</title>
   </head>
-
   <body>
     <div id="root"></div>
-    <script type="module" src="../../src/index.tsx"></script>
+    <script type="module">
+      import RefreshRuntime from 'http://localhost:3000/@react-refresh'
+      RefreshRuntime.injectIntoGlobalHook(window)
+      window.$RefreshReg$ = () => {}
+      window.$RefreshSig$ = () => (type) => type
+      window.__vite_plugin_react_preamble_installed__ = true
+    </script>
+    <script type="module" src="http://localhost:3000/@vite/client"></script>
+    <script type="module" src="http://localhost:3000/src/index.tsx"></script>
   </body>
 </html>
--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,45 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import debug from "@wbe/debug";
+const { resolve } = require("path");
+const log = debug("config:vite.config");
 
 /**
  * Vite config
  * @doc https://vitejs.dev/config/
  */
+
+// TODO less var to JS
+// TODO https://github.com/asurraa-lab/react-vite2-ts-antd/blob/master/vite.config.ts
+
 export default defineConfig(({ command, mode }) => {
+  // load env from selected .env
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
   return {
-    base: "/",
+    base: process.env.VITE_BASE_URL,
     plugins: [react()],
+    clearScreen: false,
+    logLevel: "info",
     server: {
-      open: true,
+       open: "http://localhost",
       host: "0.0.0.0",
       cors: true,
-      proxy: {
-        "/": {
-          target: "http://localhost/cher-vite/dist/front",
-          changeOrigin: true,
-          secure: false,
-        },
-      },
+      // proxy: {
+      //   "/": {
+      //     target: "http://localhost/cher-vite/dist/front",
+      //     changeOrigin: true,
+      //     secure: false,
+      //   },
+      // },
     },
     build: {
       emptyOutDir: true,
-      outDir: "dist/front/static/",
+      outDir: resolve("./dist/front/static/"),
       manifest: true,
+      rollupOptions: {
+        input: resolve("./src/index.tsx"),
+      },
     },
   };
 });
