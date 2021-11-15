bcbca2dbbc2366a2d605172fd273dc8d8574bbb4
Willy Brauner <mail@willybrauner.com>
2021-11-15 12:14:30 +0100

    Add credits

--- config/helpers/couleur.ts
+++ config/helpers/couleur.ts
@@ -1,14 +1,19 @@
 /**
+ * @credit Willy Brauner
+ * code from https://github.com/willybrauner/debug/blob/main/src/couleur.ts
+ */
+
+/**
  * couleur util API
  */
 function initCouleur() {
-  const _close = "\u001B[39m"
+  const _close = "\u001B[39m";
 
   /**
    * Wrapper for ansi 256 code
    * @param code
    */
-  const _wrapAnsi256 = (code) => `\u001B[${38};5;${code}m`
+  const _wrapAnsi256 = (code) => `\u001B[${38};5;${code}m`;
 
   /**
    * Convert RGB color to ansi 256
@@ -18,17 +23,17 @@ function initCouleur() {
    */
   const _rgbToAnsi256 = (red: number, green: number, blue: number): number => {
     if (red === green && green === blue) {
-      if (red < 8) return 16
-      if (red > 248) return 231
-      return Math.round(((red - 8) / 247) * 24) + 232
+      if (red < 8) return 16;
+      if (red > 248) return 231;
+      return Math.round(((red - 8) / 247) * 24) + 232;
     }
     return (
       16 +
       36 * Math.round((red / 255) * 5) +
       6 * Math.round((green / 255) * 5) +
       Math.round((blue / 255) * 5)
-    )
-  }
+    );
+  };
 
   /**
    * Color string with rbg color
@@ -37,19 +42,19 @@ function initCouleur() {
   const rgb =
     (r: number, g: number, b: number) =>
     (str: string): string =>
-      _wrapAnsi256(_rgbToAnsi256(r, g, b)) + str + _close
+      _wrapAnsi256(_rgbToAnsi256(r, g, b)) + str + _close;
 
   /**
    * Bold text
    * @param str
    */
-  const bold = (str: string): string => "\x1B[1m" + str + "\x1B[22m"
+  const bold = (str: string): string => "\x1B[1m" + str + "\x1B[22m";
 
   return {
     rgb,
     bold,
-  }
+  };
 }
 
-const couleur = initCouleur()
-export default couleur
+const couleur = initCouleur();
+export default couleur;
