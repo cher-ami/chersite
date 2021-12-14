d8d593b82e6d43d433aa9c4bb10a58b51a9346ee
Willy Brauner <mail@willybrauner.com>
2021-12-14 10:10:05 +0100

    Install new router version

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,10 +1,52 @@
 import css from "./App.module.less"
 import React from "react"
-import { Link, Stack } from "@cher-ami/router"
+import { Link, Stack, TManageTransitions } from "@cher-ami/router"
 
 export interface IProps {}
 
-function App(props: IProps) {
+function App(props: IProps): JSX.Element {
+  // ...
+
+  // ------------------------------------------------------------------------------- PAGE TRANSITIONS
+
+  /**
+   * Manage page tranisitons senario
+   * https://github.com/cher-ami/router/tree/main#manage-transitions
+   * @param TManageTransitions
+   * @returns Promise <void>
+   */
+  const manageTransitions = ({
+    previousPage,
+    currentPage,
+    unmountPreviousPage,
+  }: TManageTransitions): Promise<void> => {
+    return new Promise(async (resolve) => {
+      const $current = currentPage?.$element
+
+      // hide new page
+      if ($current) $current.style.visibility = "hidden"
+
+      // play out and unmount previous page
+      if (previousPage) {
+        await previousPage.playOut()
+        unmountPreviousPage()
+      }
+
+      // wait page isReady promise
+      await currentPage?.isReadyPromise?.()
+
+      // show and play in new page
+      if (currentPage) {
+        if ($current) $current.style.visibility = "visible"
+        await currentPage?.playIn()
+      }
+
+      resolve()
+    })
+  }
+
+  // ------------------------------------------------------------------------------- RENDER
+
   return (
     <div className={css.root}>
       <nav>
@@ -17,7 +59,7 @@ function App(props: IProps) {
           </li>
         </ul>
       </nav>
-      <Stack className={css.stack} />
+      <Stack className={css.stack} manageTransitions={manageTransitions} />
     </div>
   )
 }
