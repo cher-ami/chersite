c03196897727cb53fd9752141c210d35d006eb96
Pierre Gradelet <pierre.gradelet@gmail.com>
2022-10-05 09:41:26 +0200

    Composer install file refacto

--- dist/composer-install.sh
+++ dist/composer-install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-echo 'Move to front folder'
+set -e
 
-[ -d "front" ] && echo 'Move to front/ folder' && cd front
+info() { echo -e "\033[0;36m> $1\033[0m"; }
+warn() { echo -e "\033[0;33m> $1\033[0m"; }
+err() { echo -e "\033[0;31m> $1\033[0m"; exit 1; }
 
-echo 'Install composer dependencies'
-composer install
+info '> Composer install needed ?'
+
+if [ -d dist/front ]; then
+    info '> Move to front folder'
+    cd dist/front
+    if [ -f composer.json ]; then
+        info '> Yes, installing composer dependencies'
+        composer install
+    else
+        info "> Composer not needed"
+    fi
+fi
 
