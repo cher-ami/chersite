757503f0cfedd958d0acee48d4c571268c44ce58
Willy Brauner <mail@willybrauner.com>
2023-08-30 14:42:44 +0200

    Add useWindowSize hook (#137)

---
+++ apps/front/src/libs/hooks/useWindowSize.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import { isBrowser } from "@cher-ami/utils"
+
+/**
+ * Return window Size
+ */
+export const useWindowSize = (): { width: number; height: number } => {
+  const [size, setSize] = useState(
+    isBrowser()
+      ? { width: window.innerWidth, height: window.innerHeight }
+      : { width: null, height: null }
+  )
+  useEffect(() => {
+    const handler = () => {
+      setSize({ width: window.innerWidth, height: window.innerHeight })
+    }
+    handler()
+    window.addEventListener("resize", handler)
+    return () => {
+      window.removeEventListener("resize", handler)
+    }
+  }, [])
+  return size
+}
