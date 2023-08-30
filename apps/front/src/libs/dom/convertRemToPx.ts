6c92c16cb769ca139ba4bd2aaa8c1666f8b9a097
Willy Brauner <willybrauner@gmail.com>
2023-08-30 15:32:29 +0200

    Update

--- apps/front/src/libs/dom/convertRemToPx.ts
+++ apps/front/src/libs/dom/convertRemToPx.ts
@@ -1,32 +1,31 @@
 /**
- * @name convertPxFromRem
- * @decription Convert px from Rem from CSS variable calculated in the ratio.less file
- * Usefull to get the same value in px that the value we put in rem in our CSS
+ * @name convertRemToPx
+ * @description Convert px from Rem from CSS variable calculated in the ratio.less file
+ * Usefully to get the same value in px that the value we put in rem in our CSS
  * @param {number} pxValue
  * @returns {number}
  */
 
-export const convertPxFromRem = (pxValue: number): number => {
-  const baseFonSizeVariable = getRootCssVariable("--font-size")
-  const baseFontSizesPxValue = convertFontSizeCssVarToPx(baseFonSizeVariable)
-
+export const convertRemToPx = (pxValue: number): number => {
+  const baseFonSizeVariable = _getRootCssVariable("--font-size")
+  const baseFontSizesPxValue = _convertFontSizeCssVarToPx(baseFonSizeVariable)
   return pxValue * baseFontSizesPxValue
 }
 
 /**
- * @name convertFontSizeCssVarToPx
+ * @name _convertFontSizeCssVarToPx
  * @description Convert the font size variable, calculated in the ratio.less file to px
  * @param cssUnitValue
  * @returns
  */
-const convertFontSizeCssVarToPx = (cssUnitValue: string): number => {
+const _convertFontSizeCssVarToPx = (cssUnitValue: string): number => {
   const isCalc = cssUnitValue.startsWith("calc")
   const isVw = cssUnitValue.endsWith("vw")
   const isVh = cssUnitValue.endsWith("vh")
 
   if (isCalc) {
     const viewportReferenceDesktopHeight = parseFloat(
-      getRootCssVariable("--viewport-reference-desktop-height")
+      _getRootCssVariable("--viewport-reference-desktop-height")
     )
     return (1 / viewportReferenceDesktopHeight) * window.innerHeight
   }
@@ -39,12 +38,12 @@ const convertFontSizeCssVarToPx = (cssUnitValue: string): number => {
 }
 
 /**
- * @name getRootCssVariable
+ * @name _getRootCssVariable
  * @description Get CSS variable from root element
  * @param variableName
  * @returns
  */
-const getRootCssVariable = (variableName: string): string => {
+const _getRootCssVariable = (variableName: string): string => {
   const rootStyles = window.getComputedStyle(document.documentElement)
   return rootStyles.getPropertyValue(variableName)
 }
