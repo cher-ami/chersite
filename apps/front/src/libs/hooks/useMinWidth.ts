c47aff866b5fe8ec0da3ade0114d21197a13f665
Willy Brauner <willybrauner@gmail.com>
2023-08-30 14:25:08 +0200

    create useMinWidth hook

---
+++ apps/front/src/libs/hooks/useMinWidth.ts
@@ -0,0 +1,20 @@
+import {
+  getBreakpointCssVariable,
+  TBreakpoint,
+} from "~/libs/dom/getBreakpointCssVariable"
+import { browserOnly } from "@cher-ami/utils"
+import { useMatchMedia } from "~/libs/hooks/useMatchMedia"
+
+/**
+ * useMatchMedia's shortcut for min-width
+ * @param breakpoint
+ *
+ * ex:
+ *  const isMinWidthTablet = useIsMinWidth("--breakpoint-tablet")
+ *
+ */
+export const useIsMinWidth = (breakpoint: TBreakpoint): boolean => {
+  return browserOnly(() =>
+    useMatchMedia(`(min-width:${getBreakpointCssVariable(breakpoint)})`)
+  )
+}
