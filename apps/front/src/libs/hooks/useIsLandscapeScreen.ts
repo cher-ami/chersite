0360921c5a567558350d1e843bc2d57361a50790
Willy Brauner <mail@willybrauner.com>
2023-08-30 17:02:41 +0200

    Add isLandscapeScreen hook (#143)

---
+++ apps/front/src/libs/hooks/useIsLandscapeScreen.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react"
+
+/**
+ * useIsLandscapeScreen
+ * @return boolean
+ */
+export function useIsLandscapeScreen(): boolean {
+  const [isLandscapeScreen, setIsLandscapeScreen] = useState<boolean>(
+    window.innerWidth > window.innerHeight
+  )
+
+  const handler = () => {
+    setIsLandscapeScreen(window.innerWidth > window.innerHeight)
+  }
+
+  useEffect(() => {
+    handler()
+    window.addEventListener("resize", handler)
+    return () => {
+      window.removeEventListener("resize", handler)
+    }
+  }, [])
+
+  return isLandscapeScreen
+}
