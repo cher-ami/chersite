5e54232372ec72d9e22e9c677b3527d9a064a03a
Willy Brauner <willybrauner@gmail.com>
2023-08-30 14:24:43 +0200

    Improve useMatchMedia

--- apps/front/src/libs/hooks/useMatchMedia.ts
+++ apps/front/src/libs/hooks/useMatchMedia.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { browserOnly } from "@cher-ami/utils"
 
 /**
  * useMatchMedia
@@ -6,10 +7,10 @@ import { useEffect, useState } from "react"
  *  const isTabletAndMore = useMatchMedia("(min-width:768px)")
  */
 export const useMatchMedia = (value: string): boolean => {
-  const matcher = window.matchMedia(value)
-  const [match, setMatch] = useState<boolean>(matcher.matches)
+  const matcher = browserOnly(() => window.matchMedia(value))
+  const [match, setMatch] = useState<boolean>(matcher?.matches)
   useEffect(() => {
-    const handler = (): void => setMatch(matcher.matches)
+    const handler = (): void => setMatch(matcher?.matches)
     handler()
     matcher.addEventListener("change", handler)
     return () => {
