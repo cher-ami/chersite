e035802cb3279c0080498402a3871e02f875c579
Willy Brauner <willybrauner@gmail.com>
2023-09-14 17:56:32 +0200

    Update prerender for prepare WP generate

--- apps/front/prerender/prerender.ts
+++ apps/front/prerender/prerender.ts
@@ -8,8 +8,9 @@ import config from "../config/config.js"
 import { isRouteIndex } from "./helpers/isRouteIndex"
 import { ManifestParser } from "./helpers/ManifestParser"
 import { renderToPipeableStream, renderToString } from "react-dom/server"
-import { JSXElementConstructor, ReactElement } from "react"
+import { ReactElement } from "react"
 import { htmlReplacement } from "~/server/helpers/htmlReplacement"
+import { IFetchError } from "~/types/IGlobalData.js"
 
 /**
  * Prerender
@@ -17,7 +18,10 @@ import { htmlReplacement } from "~/server/helpers/htmlReplacement"
  * @param urls: Urls to generate
  * @param outDirStatic: Generation destination directory
  */
-export const prerender = async (urls: string[], outDirStatic = config.outDirStatic) => {
+export const prerender = async (
+  urls: string[],
+  outDirStatic = config.outDirStatic
+): Promise<boolean[] | IFetchError[]> => {
   const indexTemplateSrc = `${outDirStatic}/index-template.html`
 
   // copy index as template to avoid the override with the generated static index.html bellow
@@ -31,25 +35,43 @@ export const prerender = async (urls: string[], outDirStatic = config.outDirStat
   const scriptTags = ManifestParser.getScriptTagFromManifest(manifest, base)
 
   // pre-render each route
-  for (let url of urls) {
-    url = url.startsWith("/") ? url : `/${url}`
+  const renderResponses = await Promise.all(
+    urls.map(async (url) => {
+      url = url.startsWith("/") ? url : `/${url}`
+      try {
+        let responseResolve
+        let responsePromise = new Promise((resolve) => (responseResolve = resolve))
 
-    try {
-      // Request DOM
-      const dom = await render(url, scriptTags, true)
-      // create stream and generate current file when all DOM is ready
-      renderToPipeableStream(dom, {
-        onAllReady() {
-          createHtmlFile(urls, url, outDirStatic, dom)
-        },
-        onError(x) {
-          console.error(x)
-        },
-      })
-    } catch (e) {
-      console.log(e)
-    }
-  }
+        // TODO: return errors if failed
+        const domResponse = await render(url, scriptTags, true)
+        // @ts-ignore
+        if ((domResponse as IFetchError).error) {
+          // @ts-ignore
+          responseResolve(domResponse as IFetchError)
+          // @ts-ignore
+          return domResponse as IFetchError
+        }
+        renderToPipeableStream(domResponse as ReactElement, {
+          onAllReady() {
+            createHtmlFile(urls, url, outDirStatic, domResponse as ReactElement).then(
+              (r) => {
+                responseResolve(r)
+              }
+            )
+          },
+          onError(x) {
+            console.error(x)
+            responseResolve(x)
+          },
+        })
+        const response = (await responsePromise) as boolean[]
+        return response
+      } catch (e) {
+        console.error(e)
+      }
+    })
+  )
+  return renderResponses as boolean[] | IFetchError[]
 }
 
 /**
@@ -63,13 +85,20 @@ const createHtmlFile = async (
   urls: string[],
   url: string,
   outDir: string,
-  dom: ReactElement<any, string | JSXElementConstructor<any>>
-): Promise<void> => {
+  dom: ReactElement
+): Promise<boolean> => {
   // Prepare file
   if (isRouteIndex(url, urls)) url = `${url}/index`
   const routePath = path.resolve(`${outDir}/${url}`)
   const htmlFilePath = `${routePath}.html`
   // Create file
-  await mfs.createFile(htmlFilePath, htmlReplacement(renderToString(dom)))
-  console.log(chalk.green(` → ${htmlFilePath.split("static")[1]}`))
+  try {
+    await mfs.createFile(htmlFilePath, htmlReplacement(renderToString(dom)))
+    console.log(chalk.green(` → ${htmlFilePath.split("static")[1]}`))
+    return true
+  } catch (error) {
+    // Handle errors
+    console.error(error)
+    return false
+  }
 }
--- apps/front/src/types/IGlobalData.ts
+++ apps/front/src/types/IGlobalData.ts
@@ -1,7 +1,21 @@
+import { TLanguage, TRoute } from "@cher-ami/router"
+
 /**
  * Global data interface
  */
 export interface IGlobalData {
   // dictionary: TDictionary
   // ...
+}
+
+export interface IApiConfig {
+  lang: TLanguage[]
+  routes: TRoute[]
+}
+
+export type IFetchError = {
+  error: boolean
+  status: number
+  message: string
+  url: string
 }
