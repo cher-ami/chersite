1b2ec18cc3423c5a8533947b957f8b474e81b078
Willy Brauner <willybrauner@gmail.com>
2023-09-14 17:58:13 +0200

    Improve example urls fetch

--- apps/front/prerender/urls.ts
+++ apps/front/prerender/urls.ts
@@ -2,21 +2,28 @@ import fetch from "cross-fetch"
 
 /**
  *  Return available Urls of our application
- *
- *
- *
  */
 export const fetchAvailableUrls = async (): Promise<string[]> => {
   /**
    If urls come from API, fetch and return URLS instead
 
-   let data
-   try {
-    data = await fetch("urls.json").then((res) => res.json())
-   } catch (e) {
-      console.log(e)
-   }
-   return data
+       let data = []
+       try {
+        data = await fetch(import.meta.env.VITE_API_SERVER_URL + "generate-url").then((res) =>
+          res.json()
+        )
+      } catch (e) {
+        console.log(e)
+      }
+
+       // return static urls
+       // prettier-ignore
+       return new Promise(resolve => {
+       resolve([
+        "/404",
+         ...data
+      ])
+   
    */
 
   // return static urls
