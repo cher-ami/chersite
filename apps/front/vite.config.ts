e45e6b956cedcd58cdb899471e69d525b3f9e788
Bastien Cornier <bastien@cher-ami.tv>
2025-01-13 11:21:30 +0100

    Update front package versions with node 22 + fixes for vite 6 & react 19

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -83,6 +83,12 @@ export default defineConfig(({ command, mode }: ConfigEnv): UserConfig => {
       },
       postcss: {
         plugins: [autoprefixer()]
+      },
+      preprocessorOptions: {
+        scss: {
+          api: "modern-compiler",
+          silenceDeprecations: ["legacy-js-api"] // Temporary before updating scss
+        }
       }
     },
 
