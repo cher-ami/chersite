30a252c2dfcdd2f3d29f39a90da9023519e6e5db
Pierre Gradelet <pierre.gradelet@gmail.com>
2025-02-12 16:13:51 +0100

    Prerender server

--- apps/front/prerender/exe-prerender-server.ts
+++ apps/front/prerender/exe-prerender-server.ts
@@ -1,5 +1,5 @@
 import chalk from "chalk"
-import express from "express"
+import Fastify, { FastifyRequest } from "fastify"
 import * as process from "process"
 import { loadEnv } from "vite"
 import { prerender } from "./prerender"
@@ -7,14 +7,17 @@ import { fetchAvailableUrls } from "./urls"
 
 const envs = loadEnv("", process.cwd(), "")
 const port = envs.PRERENDER_SERVER_NODE_PORT || process.env.PRERENDER_SERVER_NODE_PORT
-const app = express()
 
-app.get("/generate", async (req, res) => {
-  console.log(chalk.grey("req.query"), req.query)
+const fastify = Fastify({
+  logger: false
+})
+
+fastify.get("/generate", async (request: FastifyRequest, reply) => {
+  console.log(chalk.grey("request.query"), request.query)
 
   let urlsArray
-  if (req.query?.url) {
-    urlsArray = [req.query.url]
+  if ((request?.query as any)?.url) {
+    urlsArray = [(request?.query as any)?.url]
   } else {
     try {
       urlsArray = await fetchAvailableUrls()
@@ -23,20 +26,21 @@ app.get("/generate", async (req, res) => {
     }
   }
 
-  // second arg "./static" is matching cher-ami deploy conf
-  // need to be edited if we want to start this server locally
   await prerender(urlsArray)
-  res?.send("Generated static pages: " + urlsArray.join(", "))
+  return `Generated static pages: ${urlsArray.join(", ")}`
 })
 
-app.listen(port, () => {
-  console.log("")
-  console.log(
-    `> Generate all pages      ${chalk.cyan(`http://localhost:${port}/generate`)}`
-  )
-  console.log(
-    `> Generate specific page  ${chalk.cyan(
-      `http://localhost:${port}/generate?url=/my-page-url`
-    )}`
-  )
-})
+const start = async () => {
+  try {
+    await fastify.listen({ port: parseInt(port), host: "0.0.0.0" })
+    console.log(`> Generate all pages ${chalk.cyan(`http://localhost:${port}/generate`)}`)
+    console.log(
+      `> Generate specific page  ${chalk.cyan(`http://localhost:${port}/generate?url=/my-page-url`)}`
+    )
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
